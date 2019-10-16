637681a6f7d82449ff169d7c62695b9018ee4aed
lee <zucchero@qq.com>
2019-10-16 12:31:55 +0800

    代码优化

--- wechat.py
+++ wechat.py
@@ -15,55 +15,44 @@ class WeChat:
         #在此填入消息应用的agentid
         self.AGENTID = '1'
         #指定access_token缓存文件路径
-        self.TokenFile = '/tmp/.wechat_access_token'
-
-    def _get_access_token(self):
-        _url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
-        _values = {'corpid': self.CORPID,
-                  'corpsecret': self.SECRET,
-                  }
-        _req = requests.post(_url, params=_values)
-        data = json.loads(_req.text)
-        if data['errcode'] != 0:
-            print(_req.text)
-        else:
-            return data["access_token"]
+        #指定access_token缓存文件路径
+        self.TOKENFILE = '/tmp/.wechat_access_token'
 
     def get_access_token(self):
+        url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+        values = {'corpid': self.CORPID, 'corpsecret': self.SECRET}
+        req = requests.post(url, params=values)
+        if req.status_code == 200:
+            data = json.loads(req.text)
+            if data['errcode'] == 0:
+                try:
+                    with open(self.TOKENFILE, 'w') as f:
+                        f.write('\t'.join([str(time.time()), data["access_token"]]))
+                except IOError as err:
+                    print("文件" + self.TOKENFILE + "写入出错！")
+                return data["access_token"]
+        sys.exit("请求access_token失败！")
+
+    def read_access_token(self):
         try:
-            with open(self.TokenFile, 'r') as f:
+            with open(self.TOKENFILE, 'r') as f:
                 tt, access_token = f.read().split()
         except:
-            with open(self.TokenFile, 'w') as f:
-                access_token = self._get_access_token()
-                curr_time = time.time()
-                f.write('\t'.join([str(curr_time), access_token]))
-                return access_token
+            return(self.get_access_token())
         else:
-            curr_time = time.time()
-            if 0 < curr_time - float(tt) < 7260:
+            if 0 < (time.time() - float(tt)) < 7260:
                 return access_token
             else:
-                with open(self.TokenFile, 'w') as f:
-                    access_token = self._get_access_token()
-                    f.write('\t'.join([str(curr_time), access_token]))
-                    return access_token
+                return(self.get_access_token())
 
-    def renew_access_token(self):
-        with open(self.TokenFile, 'w') as f:
-            access_token = self._get_access_token()
-            curr_time = time.time()
-            f.write('\t'.join([str(curr_time), access_token]))
-            return access_token
-
-    def send_data(self, User, Subject, Message):
+    def send_message(self, User, Subject, Message):
         try:
-            access_token = self.get_access_token()
+            access_token = self.read_access_token()
         except:
             sys.exit("获取access_token失败！")
         else:
-            _url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token
-            _data = {
+            url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='
+            data = {
                 "touser": User,
                 "msgtype": "text",
                 "agentid": self.AGENTID,
@@ -72,20 +61,25 @@ class WeChat:
                     },
                 "safe": "0"
                 }
-            _values = bytes(json.dumps(_data), 'utf-8')
-            _req = requests.post(_url, _values)
-            return(json.loads(_req.text))
+            values = bytes(json.dumps(data), 'utf-8')
+            req = requests.post(url + access_token, values)
+            response = json.loads(req.text)
+            if response['errcode'] == 42001 or response['errcode'] == 40014:
+                try:
+                    access_token = self.get_access_token()
+                except:
+                    sys.exit("更新access_token失败！")
+                else:
+                    req = requests.post(url + access_token, values)
+                    response = json.loads(req.text)
+            return response
 
 if __name__ == '__main__':
     User = sys.argv[1]
     Subject = str(sys.argv[2])
     Message = str(sys.argv[3])
     wx = WeChat()
-    log = wx.send_data(User, Subject, Message)
-    #如果access_token过期或错误，重试一次
-    if log['errcode'] == 42001 or log['errcode'] == 40014:
-        wx.renew_access_token()
-        log = wx.send_data(User, Subject, Message)
+    log = wx.send_message(User, Subject, Message)
     if log['errcode'] == 0:
         print(log['errmsg'])
     else:
